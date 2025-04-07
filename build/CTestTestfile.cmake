724ffa02eea5646408df091afba153d614d2bb66
Tark <ethantarkington@gmail.com>
2025-04-06 20:48:33 -0500

    Build Made

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/Users/Ethan/OneDrive/Documents/Programming/OS_Proj2/git_OSProj2/OS_Proj_2
+# Build directory: /mnt/c/Users/Ethan/OneDrive/Documents/Programming/OS_Proj2/git_OSProj2/OS_Proj_2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
